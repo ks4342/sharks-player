7b1d5e98cb32e8971d4ed9ddced9791f718c21a8
ks4342@srmist.edu.in <ks4342@srmist.edu.in>
2022-03-14 07:30:59 +0530

    personalising playlist

--- script.js
+++ script.js
@@ -10,25 +10,23 @@ let masterSongName = document.getElementById('masterSongName');
 let songItems = Array.from(document.getElementsByClassName('songItem'));
 
 let songs = [
-    {songName: "Warriyo - Mortals [NCS Release]", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"},
-    {songName: "Cielo - Huma-Huma", filePath: "songs/2.mp3", coverPath: "covers/2.jpg"},
-    {songName: "DEAF KEV - Invincible [NCS Release]-320k", filePath: "songs/3.mp3", coverPath: "covers/3.jpg"},
-    {songName: "Different Heaven & EH!DE - My Heart [NCS Release]", filePath: "songs/4.mp3", coverPath: "covers/4.jpg"},
-    {songName: "Janji-Heroes-Tonight-feat-Johnning-NCS-Release", filePath: "songs/5.mp3", coverPath: "covers/5.jpg"},
-    {songName: "Rabba - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/6.jpg"},
-    {songName: "Sakhiyaan - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/7.jpg"},
-    {songName: "Bhula Dena - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/8.jpg"},
-    {songName: "Tumhari Kasam - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/9.jpg"},
-    {songName: "Na Jaana - Salam-e-Ishq", filePath: "songs/4.mp3", coverPath: "covers/10.jpg"},
+    {songName: "Back in Black AC/DC", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Highway to hell AC/DC", filePath: "songs/2.mp3", coverPath: "covers/2.jpg"},
+    {songName: "Perfect - ED sheeran", filePath: "songs/3.mp3", coverPath: "covers/3.jpg"},
+    {songName: "Alone Marshmellow", filePath: "songs/4.mp3", coverPath: "covers/4.jpg"},
+    {songName: "Dance in the dark AuRa", filePath: "songs/5.mp3", coverPath: "covers/5.jpg"},
+    {songName: "Faded- Alan Wallker", filePath: "songs/2.mp3", coverPath: "covers/6.jpg"},
+    {songName: "Closer Chainsmokers", filePath: "songs/2.mp3", coverPath: "covers/7.jpg"},
+    {songName: "Beliver Imagine Dragons", filePath: "songs/2.mp3", coverPath: "covers/8.jpg"},
+    {songName: "Shape of you - ED sheeran", filePath: "songs/2.mp3", coverPath: "covers/9.jpg"},
+    {songName: "Venom Eminem", filePath: "songs/4.mp3", coverPath: "covers/10.jpg"},
 ]
 
 songItems.forEach((element, i)=>{ 
     element.getElementsByTagName("img")[0].src = songs[i].coverPath; 
     element.getElementsByClassName("songName")[0].innerText = songs[i].songName; 
 })
- 
 
-// Handle play/pause click
 masterPlay.addEventListener('click', ()=>{
     if(audioElement.paused || audioElement.currentTime<=0){
         audioElement.play();
